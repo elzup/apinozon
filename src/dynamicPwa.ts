02133bc9d34f836602abced5b6b798587681d569
elzup <guild0105@gmail.com>
2021-10-26 18:56:26 +0900

    set region

--- src/dynamicPwa.ts
+++ src/dynamicPwa.ts
@@ -1,5 +1,7 @@
 import * as functions from 'firebase-functions'
 
-export const manifest = functions.https.onRequest((req, res) => {
-  res.json({ todo: 'hello' }).end()
-})
+export const manifest = functions
+  .region('asia-northeast1')
+  .https.onRequest((req, res) => {
+    res.json({ todo: 'hello' }).end()
+  })
