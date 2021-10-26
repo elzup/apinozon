42d38758312ade4882a5150885887a5e44e7c739
elzup <guild0105@gmail.com>
2021-10-26 18:55:51 +0900

    add global ip

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,19 @@ import { manifest } from './dynamicPwa'
 
 const { info } = functions.logger
 
-export const helloWorld = functions.https.onRequest((req, res) => {
-  info('Hello logs!', { structuredData: true })
-  res.send('anozon hello')
-})
+export const helloWorld = functions
+  .region('asia-northeast1')
+  .https.onRequest((req, res) => {
+    info('Hello logs!', { structuredData: true })
+    res.send('anozon hello')
+  })
+
+export const globalip = functions
+  .region('asia-northeast1')
+  .https.onRequest((req, res) => {
+    const ip = req.header('fastly-client-ip')
+
+    res.send(ip)
+  })
 
 export { manifest }
