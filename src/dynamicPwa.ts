e76205cafbee6bf6e342b6f661fbd181b398d71c
elzup <guild0105@gmail.com>
2021-12-09 23:41:53 +0900

    update dynamicPwa

--- src/dynamicPwa.ts
+++ src/dynamicPwa.ts
@@ -1,7 +1,34 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import * as functions from 'firebase-functions'
+
+const genManifest = ({
+  name,
+  short_name,
+  src,
+  start_url = '/',
+}: Record<string, string>) => ({
+  name,
+  short_name,
+  start_url,
+  display: 'standalone',
+  theme_color: '#ffffff',
+  background_color: '#ffffff',
+  icons: [
+    {
+      src,
+      sizes: '512x512',
+      type: 'image/png',
+    },
+  ],
+})
 
 export const manifest = functions
   .region('asia-northeast1')
   .https.onRequest((req, res) => {
-    res.json({ todo: 'hello' }).end()
+    const o: Record<string, string> = {}
+
+    for (const [k, v] of Object.entries(req.query)) {
+      o[k] = v as string
+    }
+    res.json(genManifest(o)).end()
   })
