53fbba436bb8c2c9856b839bfd69af882c2c7866
elzup <guild0105@gmail.com>
2022-06-28 23:53:46 +0900

    fix notion request

--- src/notion.ts
+++ src/notion.ts
@@ -1,6 +1,7 @@
 import got from 'got'
 
 const DB_ID = process.env.NOTION_UA_CHALLANGE_DB
+const DB_TOKEN = process.env.NOTION_UA_CHALLANGE_TOKEN
 
 export const insert = async function (name: string, text: string) {
   const body = {
@@ -8,14 +9,24 @@ export const insert = async function (name: string, text: string) {
       database_id: DB_ID,
     },
     properties: {
-      Name: { title: [{ text: { content: name } }] },
-      Description: {
-        Text: [{ text: { content: text } }],
-      },
+      title: { title: [{ text: { content: name } }] },
+      Text: { text: [{ rich_text: { content: text } }] },
     },
   }
 
-  got.post('https://api.notion.com/v1/pages', {
-    json: body,
-  })
+  const res = await got
+    .post('https://api.notion.com/v1/pages', {
+      json: body,
+      headers: { authorization: `Bearer ${DB_TOKEN}` },
+    })
+    .catch((e) => {
+      console.log(e)
+      return null
+    })
+
+  if (!res) return
+
+  console.log(res.body)
 }
+
+insert('test', 'test text')
