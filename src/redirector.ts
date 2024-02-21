d704f3698559483669c756a9f2896adcf4a7e5e5
elzup <guild0105@gmail.com>
2024-02-21 18:27:17 +0900

    add redirector

---
+++ src/redirector.ts
@@ -0,0 +1,26 @@
+import { https } from './firebase'
+
+export const redirector = https.onRequest((req, res) => {
+  const url = req.query.url as string
+  if (typeof url !== 'string') {
+    res.status(400).send('url is not string')
+    return
+  }
+
+  res.send(`
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>認証ページ</title>
+	<script type="text/javascript">
+	setTimeout(() => {
+		location.href='${url}' }, 1000)
+	</script>
+</head>
+<body style="padding: 8px">
+認証中...
+</body>
+</html>
+`)
+})
